83ab6ae8e6a0461b6658c957401a1d25efa58ca1
Jack Mchugh <60944604+jackmchugh17@users.noreply.github.com>
2021-02-17 19:55:05 +0000

    Update TextGame.java
    Player level, name and class all added as well as a kill count for enemies killed.

--- TextGame.java
+++ TextGame.java
@@ -10,7 +10,6 @@
  */
 
 
-
 import java.util.Random;
 import java.util.Scanner;
 
@@ -26,6 +25,7 @@ public class TextGame {
        String[] enemies = {"Skeleton", "Goblin", "Troll", "Assassin" };
        int maxEnemyHealth = 75;
        int enemyAttackDamage = 25;
+       int killCount = 0;
        
        //Player Variables
        int health = 100;
@@ -33,15 +33,41 @@ public class TextGame {
        int numHealthPotions = 3;
        int healthPotionHealAmount = 30;
        int healthPotionDropChance = 50; //Chance in percentage 
+       int playerLevel = 0;
        
        boolean running = true;
        
        System.out.println("Welcome to the Dungeon!");
        
+       System.out.println("What will you name your character?");
+           String name = in.nextLine();
+           
+           System.out.println("Well " + name + ", what class will you choose?");
+           System.out.println("\t1. Mage");
+           System.out.println("\t2. Warrior");
+           System.out.println("\t3. Rogue");
+           String build = in.nextLine();
+           
+           if(build.equals("1")){
+               System.out.println("A mystic Mage! A master of the magical and mystic arts!");
+               
+           }
+           else if(build.equals("2")){
+               System.out.println("A brave Warrior I see! A master of battle and strategy!");
+           }
+          else if(build.equals("3")){
+               System.out.println("A cunning Rogue! A master of stealth and guile!");
+           }
+           else{
+               System.out.println("Invalid command!");
+           }
+       
       
        GAME:
        while(running) {
            System.out.println("-------------------------------------");
+           
+           
            
            int enemyHealth = rand.nextInt(maxEnemyHealth);
            String enemy = enemies[rand.nextInt(enemies.length)];
@@ -50,7 +76,7 @@ public class TextGame {
            while(enemyHealth > 0) {
                System.out.println("\tYour HP: " + health);
                System.out.println("\t" + enemy + "'s HP: " + enemyHealth);
-               System.out.println("\n\tWhat would you like to do?");
+               System.out.println("\n\tWhat would you like to do " + name + "?");
                System.out.println("\t1. Attack");
                System.out.println("\t2. Drink health potion");
                System.out.println("\t3. Run!");
@@ -68,7 +94,7 @@ public class TextGame {
                    System.out.println("\t> You receive " + damageTaken + " in retaliation!");
                    
                    if(health <1) {
-                       System.out.println("\'t> You have taken too much damage, you are too weak to carry on! ");
+                       System.out.println("\t> You have taken too much damage, you are too weak to carry on! ");
                        break;
                        
                    }
@@ -98,20 +124,32 @@ public class TextGame {
            
            if(health < 1) {
                System.out.println("You limp out of the dungeon, weak from battle.");
+               System.out.println("Though " + name + " was defeated they killed " + killCount + " enemies. A valiant effort from our hero");
                break;
            }
            
             System.out.println("-------------------------------------");
             System.out.println(" # " + enemy + " was defeated! #");
             System.out.println(" # You have " + health + " HP left. #");
+            System.out.println(" # Your level is " + playerLevel);
             if(rand.nextInt(100) < healthPotionDropChance) {
                 numHealthPotions++;
                 System.out.println(" # The " + enemy + " dropped a health potion! #");
                 System.out.println(" # You now have " + numHealthPotions + " health potion(s). # ");
                 
             }
+            if(enemyHealth < 0)
+            {
+                killCount++;
+                System.out.println(" # You have defeated " + killCount + " enemies");
+            }
+            if(killCount % 2 == 0){               
+                playerLevel++;
+                System.out.println(" # LEVEL UP! Congrats you are now level " + playerLevel);
+            }
+           
            System.out.println("-------------------------------------");
-           System.out.println("What you like to do now?");
+           System.out.println("What you like to do now " + name + "?");
            System.out.println("1. Continue fighting");
            System.out.println("2. Exit dungeon");
            
@@ -127,6 +165,7 @@ public class TextGame {
            }
            else if(input.equals("2")) {
                System.out.println("You exit the dungeon, successful from your adventures!");
+               System.out.println(name + " successfully defeated " + killCount +  " enemies. Good hunting!!");
                break;
            }
                    
